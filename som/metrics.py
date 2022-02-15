38e270de38658188b0d10ed8c1217cc340eda04f
Jonas Grebe <26788162+jonasgrebe@users.noreply.github.com>
2022-02-15 17:28:43 +0100

    Update metrics.py

--- som/metrics.py
+++ som/metrics.py
@@ -4,11 +4,14 @@ import numpy as np
 def get_metric(metric: str):
     metric = metric.lower()
 
-    if metric in ['l1', 'manhattan']:
+    if metric in ['l1', 'manhattan', 'taxicab']:
         return manhattan_distance
 
     if metric in ['l2', 'euclidian']:
         return euclidian_distance
+
+    if metric in ['inf', 'max', 'infinity', 'maximum']:
+        return infinity_norm
 
 
 def manhattan_distance(x, y):
@@ -17,3 +20,7 @@ def manhattan_distance(x, y):
 
 def euclidian_distance(x, y):
     return np.sqrt(np.square(x - y).sum(axis=-1))
+
+
+def infinity_norm(x, y):
+    return np.max(np.abs(x - y), axis=-1)
